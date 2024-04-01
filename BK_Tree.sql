8aa290d8776c3c9a335d9bf664575a0cfcbd782e
sfelman <sfelman@eckerd.edu>
2024-04-01 14:54:16 -0400

    Update BK_Tree.sql

--- BK_Tree.sql
+++ BK_Tree.sql
@@ -4,7 +4,7 @@ GO
 CREATE TABLE dbo.BK_Tree(
 	id int IDENTITY(1,1) NOT NULL CONSTRAINT [PK_BK_Tree_id] PRIMARY KEY CLUSTERED,
 	word nvarchar(50) NOT NULL CONSTRAINT [UQ_BK_Tree_word] UNIQUE,
-	parent_node_id int NULL,
+	parent_node_id int NULL REFERENCES BK_Tree(id),
 	levenshtein_distance int NULL,
 	active bit NOT NULL DEFAULT 1
 );
