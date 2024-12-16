6c16cdfe0ea1847f14a1fec7a86bcca77a83ad05
sfelman <sfelman@eckerd.edu>
2024-12-15 21:32:57 -0500

    Update BK_Tree.sql
    added bredth-first clustered index that speeds up both insert and search.

--- BK_Tree.sql
+++ BK_Tree.sql
@@ -1,11 +1,13 @@
 USE [Sandbox]
 GO
-	
+
 CREATE TABLE dbo.BK_Tree(
-	id int IDENTITY(1,1) NOT NULL CONSTRAINT [PK_BK_Tree_id] PRIMARY KEY CLUSTERED,
+	id int IDENTITY(1,1) NOT NULL CONSTRAINT [PK_BK_Tree_Id] PRIMARY KEY NONCLUSTERED,
 	word varchar(50) NOT NULL CONSTRAINT [UQ_BK_Tree_word] UNIQUE,
-	parent_node_id int NULL,
+	parent_node_id int NULL constraint [FK_BK_Tree_Parent_Node_Id_BK_Tree_Id] FOREIGN KEY (Parent_Node_Id) references BK_Tree(id),
 	levenshtein_distance int NULL,
 	active bit NOT NULL DEFAULT 1
 );
 GO
+
+CREATE CLUSTERED INDEX [IX_BK_Tree_Breadth_First] on BK_Tree(Parent_Node_Id, Id)
