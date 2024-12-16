53828767bdafe86fa78ac501cc01c73a4b036f19
sfelman <sfelman@eckerd.edu>
2024-12-16 14:19:47 -0500

    Update P_BK_Tree_Search.sql
    changed name of cte for clarity

--- P_BK_Tree_Search.sql
+++ P_BK_Tree_Search.sql
@@ -13,19 +13,19 @@ CREATE PROCEDURE [dbo].[P_BK_Tree_Search]
 AS
 BEGIN
 
-	;with q as (
+	;with candidate_nodes as (
 		select id, word, active
 		from BK_Tree 
 		where id = 1
 		union all
 		select b.id, b.word, b.active
-		from q
-		join BK_Tree b on q.id = b.parent_node_id
-		where b.levenshtein_distance >= dbo.F_Levenshtein(q.word, @search_word) - @tolerance
-		and b.levenshtein_distance <= dbo.F_Levenshtein(q.word, @search_word) + @tolerance
+		from candidate_nodes cn
+		join BK_Tree b on cn.id = b.parent_node_id
+		where b.levenshtein_distance >= dbo.F_Levenshtein(cn.word, @search_word) - @tolerance
+		and b.levenshtein_distance <= dbo.F_Levenshtein(cn.word, @search_word) + @tolerance
 	)
 	select id, word, 1 - (dbo.F_Levenshtein(word,@search_word)*1.0)/(GREATEST(len(word),len(@search_word))*1.0) [similarity_percent]
-	from q 
+	from candidate_nodes
 	where dbo.F_Levenshtein(word, @search_word) <= @tolerance
 	and active = 1
 	order by 1 - (dbo.F_Levenshtein(word,@search_word)*1.0)/(GREATEST(len(word),len(@search_word))*1.0) desc
