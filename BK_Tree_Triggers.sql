2e25bdfc547b9bd67e1a3131a882bbc44e1efdf0
sfelman <sfelman@eckerd.edu>
2024-12-15 21:38:07 -0500

    Update BK_Tree_Triggers.sql
    new insert trigger. Replaces the old insert trigger and also the P_BK_Tree_Insert stored procedure.

--- BK_Tree_Triggers.sql
+++ BK_Tree_Triggers.sql
@@ -1,54 +1,65 @@
 USE Sandbox
 GO
 
-CREATE TRIGGER dbo.TR_BK_Tree_INSERT
-ON dbo.BK_Tree
-INSTEAD OF INSERT
-AS
-BEGIN
-CREATE TABLE #stage
-(
-	id int IDENTITY(1,1),
-	word varchar(50),
-	active bit
-)
-insert into #stage
-select word, active from inserted
-DECLARE @i int = 1, 
-		@total int = (select count(*) from #stage),
-		@insert varchar(50),
-		@active bit
---Note: This while loop is time intensive. In my testing, it does about 80,000 inserts per hour but results may vary.
-WHILE (@i <= @total)
-	BEGIN
-		SELECT @insert = word, @active = active FROM #stage WHERE id = @i
-		--resets active_flag to 1 if word already in bk_tree structure, this also avoids duplicates of same word.
-		IF(exists (select 1 from BK_TREE where word = @insert))
-		BEGIN
-			update BK_Tree set active = 1 where word = @insert
-		END
-		--otherwise insert the new word
-		ELSE
-		BEGIN
-			EXEC P_BK_Tree_Insert @insert_word = @insert, @active_flag = 1
-		END
-		SET @i = @i+1
-	END 
-RETURN
-END;
-GO
 
 CREATE TRIGGER dbo.TR_BK_Tree_UPDATE
 ON dbo.BK_Tree
 AFTER UPDATE
 AS
 BEGIN
-IF EXISTS (select 1 from inserted i join deleted d on i.id = d.id where i.word != d.word OR i.levenshtein_distance != d.levenshtein_distance OR i.parent_node_id != d.parent_node_id OR (d.levenshtein_distance IS NULL AND i.levenshtein_distance IS NOT NULL) OR (d.parent_node_id IS NULL AND i.parent_node_id IS NOT NULL))
+IF EXISTS (SELECT 1 FROM inserted i join deleted d on i.id = d.id WHERE i.word != d.word OR i.levenshtein_distance != d.levenshtein_distance OR i.parent_node_id != d.parent_node_id OR (d.levenshtein_distance IS NULL AND i.levenshtein_distance IS NOT NULL) OR (d.parent_node_id IS NULL AND i.parent_node_id IS NOT NULL))
 	BEGIN
 	RAISERROR ('Cannot Update Table. Only Active Flag can be Updated.',-1,-1,'BK_Tree')
 	ROLLBACK TRANSACTION;
 	RETURN
 	END
+END;
+GO
+
+
+CREATE TRIGGER dbo.TR_BK_Tree_INSERT
+ON dbo.BK_Tree
+AFTER INSERT
+AS
+BEGIN
+
+ALTER TABLE BK_Tree DISABLE TRIGGER TR_BK_Tree_UPDATE
+
+--variables that for calculating the parent_node_id
+DECLARE @insert_word varchar(50),
+		@insert_word_id int,
+		@parent_node_id int = 1,
+		@parent_word varchar(50) = (SELECT word FROM BK_Tree WHERE id = 1),
+		@parent_levenshtein_distance int,
+		@conflict_id int
+
+--null parent_node_id means that the row has not yet been added to the tree. (except for the root node id = 1, all should have a parent_node_id)
+WHILE exists(SELECT 1 FROM BK_Tree WHERE parent_node_id is null and id != 1)
+BEGIN
+	if(@insert_word is null)
+	BEGIN
+		SELECT top 1 @insert_word = word, @insert_word_id = id FROM BK_Tree WHERE parent_node_id is null and id != 1 ORDER BY id ASC
+	END
+	set @parent_levenshtein_distance = (select dbo.F_Levenshtein(@parent_word,@insert_word))
+	set @conflict_id = (select id from BK_Tree where parent_node_id = @parent_node_id and levenshtein_distance = @parent_levenshtein_distance)
+	--no confliction, update the parent node
+	if @conflict_id is null
+	BEGIN
+		UPDATE BK_tree set parent_node_id = @parent_node_id, levenshtein_distance = @parent_levenshtein_distance WHERE id = @insert_word_id
+		set @parent_node_id = 1 
+		set @parent_word = (SELECT word FROM BK_Tree WHERE id = 1)
+		set @insert_word = null
+	END
+	--confliction for insert, try again with child node
+	ELSE
+	BEGIN
+		set @parent_node_id = @conflict_id
+		set @parent_word = (SELECT word FROM BK_Tree WHERE id = @conflict_id)
+	END
+END
+
+ALTER TABLE BK_Tree ENABLE TRIGGER TR_BK_Tree_UPDATE;
+RETURN
 END;
 GO
 
@@ -58,7 +69,7 @@ INSTEAD OF DELETE
 AS
 BEGIN
 	RAISERROR ('Cannot Delete - Will destroy structure of BK_Tree. Updating Active Flag to 0 instead.',-1,-1,'BK_Tree')
-	UPDATE BK_Tree set active = 0 WHERE word in (select word from deleted)
+	UPDATE BK_Tree set active = 0 WHERE word in (SELECT word FROM deleted)
 	RETURN
 END;
 GO
